1b24aa502c7677d5dd1cd33afcd736fcb4654f2c
Rasmus Olsson <rasmus.olsson2@yh.nackademin.se>
2024-03-28 14:28:08 +0100

    test

---
+++ Lektion4/script.js
@@ -0,0 +1,13 @@
+// alert("Hello World!")
+
+function myFunc() {
+    console.log("Hello Events");
+    let h1 = document.getElementById("heading");
+    h1.innerHTML = "Hello Events";
+    h1.classList.add('text-danger');
+  }
+
+  function showTime() {
+    document.getElementById("time").innerHTML =
+      new Date().toLocaleTimeString();
+  }
