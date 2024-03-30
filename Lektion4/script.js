fb3e67b3cb8688e559a54577ea75fc5906c33755
Rasmus Olsson <rasmus.olsson2@yh.nackademin.se>
2024-03-30 10:49:23 +0100

    some more files

--- Lektion4/script.js
+++ Lektion4/script.js
@@ -1,13 +1,12 @@
 // alert("Hello World!")
 
 function myFunc() {
-    console.log("Hello Events");
-    let h1 = document.getElementById("heading");
-    h1.innerHTML = "Hello Events";
-    h1.classList.add('text-danger');
-  }
-
-  function showTime() {
-    document.getElementById("time").innerHTML =
-      new Date().toLocaleTimeString();
-  }
+  console.log("Hello Events");
+  let h1 = document.getElementById("heading");
+  h1.innerHTML = "Hello Events";
+  h1.classList.add("text-primary");
+}
+function showTime() {
+  document.getElementById("time").innerHTML =
+    new Date().toLocaleTimeString();
+}
